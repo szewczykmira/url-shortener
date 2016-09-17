f1680779b6a1002803e0689768a2cd43a77a0a15
Mira <szewczyk.mira@gmail.com>
2016-09-17 16:24:56 +0200

    Secret key moved to os

--- shortener/settings/settings.py
+++ shortener/settings/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+# set it from command line -> epxort SECRET_KEY="somestring"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
