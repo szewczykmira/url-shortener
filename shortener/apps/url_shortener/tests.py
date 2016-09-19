3506cba349e880c29b331c451582cbb1468bdfef
Mira <szewczyk.mira@gmail.com>
2016-09-19 15:53:31 +0200

    Tests added

--- shortener/apps/url_shortener/tests.py
+++ shortener/apps/url_shortener/tests.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import  reverse
 from django.test import TestCase
 
 
 class ShortenerTest(TestCase):
     def test_view_home(self):
-        pass
+        response = self.client.get(reverse('home'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'url_shortener/home.html')
