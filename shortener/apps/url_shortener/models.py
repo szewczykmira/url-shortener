4b7c01e174187b35873810ffb0c2167cea2cf476
Mira <szewczyk.mira@gmail.com>
2016-09-20 13:42:29 +0200

    ShortURL model with migrations added

--- shortener/apps/url_shortener/models.py
+++ shortener/apps/url_shortener/models.py
@@ -1,5 +1,28 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+@python_2_unicode_compatible
+class ShortURL(models.Model):
+    original_url = models.URLField(verbose_name=_("Original url"),
+                                   editable=False, unique=True)
+    short_url = models.TextField(verbose_name=_("Short url"))
+    user = models.ForeignKey(User, verbose_name=_("Submitter"))
+
+    class Meta:
+        ordering = ['original_url']
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            pass
+        super(ShortURL, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.original_url
