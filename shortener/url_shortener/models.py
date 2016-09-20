699d46e983749dccbd089dfec374af00cb435f4a
Mira <szewczyk.mira@gmail.com>
2016-09-20 23:28:15 +0200

    Bad code removed

--- shortener/url_shortener/models.py
+++ shortener/url_shortener/models.py
@@ -11,12 +11,6 @@ import base64
 User = settings.AUTH_USER_MODEL
 
 
-def convert(origin):
-    max = settings.SHORT_URL_MAX_LENGTH
-    base64_str = base64.urlsafe_b64encode(origin)
-    return base64_str[:max] if len(base64_str) > max else base64_str
-
-
 @python_2_unicode_compatible
 class ShortURL(models.Model):
     original_url = models.URLField(verbose_name=_("Original url"),
@@ -29,16 +23,6 @@ class ShortURL(models.Model):
 
     def __str__(self):
         return self.original_url
-
-    def set_short_url(self):
-        # TODO: test
-        all_shortend = ShortURL.objects.all().values_list('short_url',
-                                                          flat=True)
-        base64_str = convert(self.original_url[::-1])
-
-        while base64_str in all_shortend:
-            base64_str = convert(str(self.pk) + base64_str)
-        self.short_url = base64_str
 
     def save(self, *args, **kwargs):
         self.full_clean()
