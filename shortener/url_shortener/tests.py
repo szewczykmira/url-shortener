a69e043cffae31d0c54d7218f051b14e81d19150
Mira <szewczyk.mira@gmail.com>
2016-09-20 19:15:30 +0200

    Tests added

--- shortener/url_shortener/tests.py
+++ shortener/url_shortener/tests.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from models import ShortURL
-from users import utils
+from users.utils import get_random_user
 
 
 class ShortenerTest(TestCase):
@@ -16,14 +16,37 @@ class ShortenerTest(TestCase):
         self.assertTemplateUsed(response, 'url_shortener/home.html')
 
     def test_model_must_have_origin_url(self):
-        user = utils.get_random_user()
+        user = get_random_user()
         count = ShortURL.objects.all().count()
         has_error = False
         try:
-            obj = ShortURL(user=user, short_url="ala")
+            obj = ShortURL(user=user, short_url="foo")
             obj.save()
         except ValidationError:
             has_error = True
 
+        self.assertTrue(has_error)
+        self.assertEqual(count, ShortURL.objects.all().count())
+
+    def test_model_must_have_user(self):
+        count = ShortURL.objects.all().count()
+        has_error = False
+        try:
+            obj = ShortURL(short_url="foo", original_url="bar")
+            obj.save()
+        except ValidationError:
+            has_error = True
+        self.assertTrue(has_error)
+        self.assertEqual(count, ShortURL.objects.all().count())
+
+    def test_model_must_have_short_url(self):
+        user = get_random_user()
+        count = ShortURL.objects.all().count()
+        has_error = False
+        try:
+            obj = ShortURL(original_url="foo", user=user)
+            obj.save()
+        except ValidationError:
+            has_error = True
         self.assertTrue(has_error)
         self.assertEqual(count, ShortURL.objects.all().count())
