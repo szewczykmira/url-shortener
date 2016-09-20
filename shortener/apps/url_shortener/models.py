732f77898178c55c6ff87ac0ef68cf1fcfd2c8b3
Mira <szewczyk.mira@gmail.com>
2016-09-20 14:52:14 +0200

    Changes in model

--- shortener/apps/url_shortener/models.py
+++ shortener/apps/url_shortener/models.py
@@ -6,23 +6,24 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 
-
 User = settings.AUTH_USER_MODEL
+
 
 @python_2_unicode_compatible
 class ShortURL(models.Model):
     original_url = models.URLField(verbose_name=_("Original url"),
-                                   editable=False, unique=True)
-    short_url = models.TextField(verbose_name=_("Short url"))
+                                   unique=True)
+    short_url = models.TextField(verbose_name=_("Short url"), unique=True)
     user = models.ForeignKey(User, verbose_name=_("Submitter"))
 
     class Meta:
         ordering = ['original_url']
 
     def save(self, *args, **kwargs):
-        if not self.pk:
-            pass
         super(ShortURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.original_url
+
+    def set_short_url(self):
+        pass
