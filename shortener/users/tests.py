07b16da443db289240fa960ee46c7a0a74fbe470
Mira <szewczyk.mira@gmail.com>
2016-10-21 18:41:44 +0200

    Assert rises in tests

--- shortener/users/tests.py
+++ shortener/users/tests.py
@@ -16,28 +16,12 @@ class CreateFakeUsersTest(TestCase):
         self.out = StringIO()
 
     def test_no_arguments(self):
-        error_rise = False
-        try:
+        with self.assertRaises(CommandError):
             call_command('create_fake_users', stdout=self.out)
-        except CommandError:
-            error_rise = True
-        self.assertTrue(error_rise)
 
     def test_wrong_argument_type(self):
-        error_rise = False
-        try:
+        with self.assertRaises(CommandError):
             call_command('create_fake_users', "wq", stdout=self.out)
-        except CommandError:
-            error_rise = True
-        self.assertTrue(error_rise)
-
-    def test_string_as_int(self):
-        try:
-            call_command("create_fake_users", "12", stdout=self.out)
-            error_rise = False
-        except CommandError:
-            error_rise = True
-        self.assertFalse(error_rise)
 
     def test_create_users(self):
         count = User.objects.all().count()
